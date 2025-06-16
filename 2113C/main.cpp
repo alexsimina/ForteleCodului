c89591dee2ac1a9800dae1314811201720c7c994
alex44 <alisimina10@gmail.com>
2025-06-16 19:04:21 +0300

    ceva

--- 2113C/main.cpp
+++ 2113C/main.cpp
@@ -6,6 +6,22 @@ int t, n, m, k, minGoldLost, tGold;
 //0 - empty
 //1 - gold
 //-1 blocked
+int map[509][509];
+
+int obj(char& x)
+{
+    switch (x)
+    {
+        case '#':
+            return -1;
+        case '.':
+            return 0;
+        case 'g':
+            return 1;
+    }
+
+    return -2;
+}
 
 int main()
 {
